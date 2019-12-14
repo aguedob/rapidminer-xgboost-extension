4ad92d1d365b41e6a753c765b1919de567d627ff
Andrés Guerrero Doblado <andres@meigal.com>
2019-12-14 15:24:14 +0100

    Cleanup code

--- src/com/rapidminer/operator/learner/functions/XGBRegressionMethod.java
+++ src/com/rapidminer/operator/learner/functions/XGBRegressionMethod.java
@@ -17,16 +17,7 @@ public interface XGBRegressionMethod {
 		public double error;
 		public boolean[] isUsedAttribute;
 	}
-	
-	/**
-	 * This method performs the actual regression. There are passed the linear regression operator itself as well as data 
-	 * and it's properties. Before this method is called, the linear regression already has performed a regression on 
-	 * the full data set. This resulted in the given coefficients. Please note, that if useBias is true,
-	 * the last coefficient is the bias.
-	 * @throws UndefinedParameterError 
-	 */
-//	public LinearRegressionResult applyMethod(LinearRegression regression, boolean useBias, double ridge, ExampleSet exampleSet, boolean[] isUsedAttribute, int numberOfExamples, int numberOfUsedAttributes, double[] means, double labelMean, double[] standardDeviations, double labelStandardDeviation, double[] coefficientsOnFullData, double errorOnFullData) throws UndefinedParameterError;
-	
+		
 	/**
 	 * This method must return a List of needed Parameters.
 	 */
